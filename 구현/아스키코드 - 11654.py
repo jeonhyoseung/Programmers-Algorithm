345a7332d8606ecd54de97679c8c10f2b07955fc
jeonhyoseung <thehube2020@gmail.com>
2022-03-19 23:52:05 +0900

    수학 : 11720, 구현 : 11654

---
+++ 구현/아스키코드 - 11654.py
@@ -0,0 +1,4 @@
+asc = input()
+print(ord(asc))
+# TIL : 1) 아스키 코드 바꾸는 함수 -> ord
+#  2) chr 함수는 아스키 코드 -> 문자
---
+++ 수학/숫자의 합 - 11720.py
@@ -0,0 +1,6 @@
+a = int(input())
+n = list(input())
+sum = 0
+for i in n:
+    sum += int(i)
+print(sum)
