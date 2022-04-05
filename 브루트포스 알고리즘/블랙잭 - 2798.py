338d79443b9a7ea13a70ba72aa73dc1c733c806a
Stark <77372040+jeonhyoseung@users.noreply.github.com>
2022-04-05 18:57:21 +0900

    docs(브루트포스) : clearly document how to solve bruteforce algoithm

--- 브루트포스 알고리즘/블랙잭 - 2798.py
+++ 브루트포스 알고리즘/블랙잭 - 2798.py
@@ -1,27 +1,23 @@
-n, m = map(int, input().split())
+N, M = map(int,input().split())
 numlist = list(map(int, input().split()))
-
 answer = 0
-
 for i in range(len(numlist)):
-    for j in range(i + 1, len(numlist)):
-        for k in range(j + 1, len(numlist)):
+    for j in range(i+1, len(numlist)):
+        for k in range(j+1, len(numlist)):
             sum = numlist[i] + numlist[j] + numlist[k]
-            if sum <= m:
-                answer = max(answer, sum)
-print(answer)
- 
-# 딜러는 N장의 카드를 숫자가 보이게
-# ->
-# M을 외치면
-# ->
-# N장의 카드 중 3장을 골라야 함
-# ->
-# M을 넘지 않으면서 M과 가깝게 3장을 골라야 함
+            if sum <=M:
+                answer = max(answer,sum)
 
-# 입력
-# N M
-# A1, A2 --------AN
+print(answer)  
+# 문제 설명 _ 
+# N : 카드 개수
+# M : 원하는 합
+# 2 line : 카드의 수
+# 출력 -> 3장 뽑았을때 M에 가장 가까운 수
 
-# 출력
-# M에 최대한 가까운 카드 3장의 합
+# 해설 설명 : 브루트포스 알고리즘 (무식한 방식의 완전탐색 알고리즘) 
+# Line 2) 받은 입력 값을 lst화 해서 정리
+# Line 4~  3중포문을 사용하여 모든 경우를 따져서 M보다 작거나 같으면 answer에 넣고
+# answer에서 최대값을 고르고 맨 마직 for문에서의 answer를 출력
+# 가령 카드의 수가 5,6,7,8,9 일 경우
+# (5,6,7) , (5,6,8), (5,6,9), (5,7,8),(5,7,9),(6,7,8) 로 for 문이 진행 됨
